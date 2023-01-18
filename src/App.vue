ab4712526a34a084839c31a9d8c9ec1653bf0f2f
Evren Yeniev <mail@evren.dev>
2023-01-18 10:51:11 +0300

    Calculation bugs fixed

--- src/App.vue
+++ src/App.vue
@@ -13,31 +13,30 @@ export default {
     KeypadComponent,
   },
   setup() {
-    let num1 = ref(0);
-    let num2 = ref(0);
+    let num1 = ref("0");
+    let num2 = ref("0");
     let process = ref(null);
     let display = ref("0");
-    let comma = ref(false);
     let err = ref(false);
 
     const result = {
       "+": () => {
-        return (num1.value * 10 + num2.value * 10) / 10;
+        return (parseFloat(num1.value) * 10 + parseFloat(num2.value) * 10) / 10;
       },
       "-": () => {
-        return num1.value - num2.value;
+        return parseFloat(num1.value) - parseFloat(num2.value);
       },
       "/": () => {
-        if (num2.value == 0) {
+        if (parseFloat(num2.value) == 0) {
           err.value = true;
           num1.value = 0;
           return;
         }
 
-        return num1.value / num2.value;
+        return parseFloat(num1.value) / parseFloat(num2.value);
       },
       "*": () => {
-        return num1.value * num2.value;
+        return parseFloat(num1.value) * parseFloat(num2.value);
       },
       "=": () => {
         return num1.value;
@@ -45,59 +44,46 @@ export default {
     };
 
     const numericKeyPressed = (key) => {
-      if (!process.value && key != ".") {
-        num1.value =
-          comma.value && Number.isInteger(num1.value)
-            ? parseFloat(`${num1.value}.${key}`)
-            : parseFloat(`${num1.value}${key}`);
-        display.value = num1.value.toString();
-      } else if (!process.value && key == "." && !comma.value) {
-        comma.value = true;
-        display.value = `${num1.value}.`;
-      } else if (process.value && key != ".") {
-        num2.value =
-          comma.value && Number.isInteger(num2.value)
-            ? parseFloat(`${num2.value}.${key}`)
-            : parseFloat(`${num2.value}${key}`);
-        display.value = num2.value.toString();
-      } else if (process.value && key == "." && !comma.value) {
-        comma.value = true;
-        display.value = `${num2.value}.`;
+      if (!process.value) {
+        const comma = /\./g.test(num1.value);
+        num1.value +=
+          (comma && key == ".") || (!comma && parseInt(num1.value) == 0 && key == "0") ? "" : key;
+        display.value = num1.value;
+      } else {
+        const comma = /\./g.test(num2.value);
+        num2.value +=
+          (comma && key == ".") || (!comma && parseInt(num2.value) == 0 && key == "0") ? "" : key;
+        display.value = num2.value;
       }
     };
 
     const processKeyPressed = (key) => {
       if ((key == "+" || key == "-" || key == "*" || key == "/" || key == "=") && !process.value) {
         num2.value = 0;
-        comma.value = false;
         process.value = key;
       } else if (
         (key == "+" || key == "-" || key == "*" || key == "/" || key == "=") &&
         process.value
       ) {
         num1.value = result[process.value]();
-        num2.value = 0;
-        comma.value = false;
+        num2.value = "0";
         process.value = key;
-      } else if (key == "del" && num2.value > 0) {
-        num2.value = num2.value > 0 ? parseFloat(`${num2.value}`.slice(0, -1)) : 0;
-        comma.value = !Number.isInteger(num2.value);
-      } else if (key == "del" && num2.value == 0 && num1.value > 0) {
-        num1.value = num1.value > 0 ? parseFloat(`${num1.value}`.slice(0, -1)) : 0;
-        comma.value = !Number.isInteger(num1.value);
+      } else if (key == "del" && process.value && num2.value != "0") {
+        num2.value = `${num2.value}`.slice(0, -1);
+      } else if (key == "del" && !process.value && num1.value != "0") {
+        num1.value = `${num1.value}`.slice(0, -1);
       } else if (key == "reset") {
-        num1.value = 0;
-        num2.value = 0;
+        num1.value = "0";
+        num2.value = "0";
         err.value = false;
-        comma.value = false;
         process.value = null;
       }
 
       display.value = err.value
         ? "ERROR"
-        : num2.value > 0
+        : num2.value.toString() != "0"
         ? num2.value.toString()
-        : Number(num1.value).toString();
+        : num1.value.toString();
     };
 
     return { numericKeyPressed, processKeyPressed, display };
